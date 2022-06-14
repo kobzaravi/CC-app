63646f913131b90f0088457f2b0e18828efe85a5
Oleksandr Kobzar <kobzaravi@gmail.com>
2022-06-14 06:46:04 +0300

    bootstap

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -8,11 +8,11 @@ export class CalcComponent implements OnInit {
   result!:number;
   
   constructor() { }
-  Calculate(val1:string,val2:string){
-    this.result=parseFloat(val1)*parseFloat(val2);
+  Calculate(val1:string,val2:string,val3:string){
+    this.result=parseFloat(val1)*parseFloat(val2)*parseFloat(val3);
   }
   ngOnInit(): void {
   }
-
+  
 }
 
